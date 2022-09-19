6f86ab3edcfbad4eecba0dd1842020153fe2f8ad
hiro0822 <107989420+hiro0822@users.noreply.github.com>
2022-09-19 03:38:06 +0000

    チャット一覧途中

--- app/controllers/chatgroups_controller.rb
+++ app/controllers/chatgroups_controller.rb
@@ -2,12 +2,19 @@ class ChatgroupsController < ApplicationController
 
   def index
     @chatgroups = Chatgroup.all
+    @chatgroup_join = Connect.where(user_id: current_user.id)
+    @nogroup = "参加しているグループはありません"
   end
-  
+
+  def show
+    @chatgroup = Chatgroup.find(params[:id])
+  end
+
+
   def new
     @chatgroup = Chatgroup.new
-  end  
-  
+  end
+
   def create
     @chatgroup = Chatgroup.new(chatgroup_params)
       if @chatgroup.save
@@ -15,11 +22,27 @@ class ChatgroupsController < ApplicationController
       else
       render new,alert: 'チャットグループ作成に失敗しました'
       end
-  end  
-  
+  end
+
+  def edit
+    @chatgroup = Chatgroup.find(params[:id])
+  end
+
+  def update
+    @chatgroup = Chatgroup.find(params[:id])
+    @chatgroup.update(chatgroup_params)
+    redirect_to chatgroup_path(@chatgroup)
+  end
+
+  def destroy
+    @chatgroup = Chatgroup.find(params[:id])
+    @chatgroup.destroy
+    redirect_to chatgroups_path
+  end
+
   private
-  
+
   def chatgroup_params
-    params.require(:chatgroup).permit(:group_name,:introduction,:group_image)
-  end  
+    params.require(:chatgroup).permit(:group_name,:introduction,:group_image,user_ids: [])
+  end
 end
